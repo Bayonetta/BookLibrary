9fa3d4e7537b5ff3434e3990bd80bb60b5885abc
Bayonetta <zs18771050656@gmail.com>
2013-09-11 02:37:01 +0800

    add index

--- book.py
+++ book.py
@@ -1,20 +1,29 @@
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for,  abort, render_template, flash
-
+import time
+from sqlite3 import dbapi2 as sqlite3
+from hashlib import md5
+from datetime import datetime
+from flask import Flask, request, session, url_for, redirect, \
+     render_template, abort, g, flash, _app_ctx_stack
+from werkzeug import check_password_hash, generate_password_hash
 
 
 #CONFIGURATION
 DATABASE = '/tmp/book.db'
 DEBUG = True
 SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'defualt'
-
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
+
+
+@app.route('/')
+def index():
+        return render_template('index.html')
+
 if __name__ == '__main__':
-    app.run()
+	app.run()
+
+
+
