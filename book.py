f4c4d6bd8ed724cd7009740aafe7241b1da8252d
Bayonetta <zs18771050656@gmail.com>
2013-09-13 02:02:40 +0800

    add register

--- book.py
+++ book.py
@@ -32,6 +32,11 @@ def reader_login():
 	return render_template('login.html')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	return render_template('register.html')
+
+
 if __name__ == '__main__':
 	app.run()
 
